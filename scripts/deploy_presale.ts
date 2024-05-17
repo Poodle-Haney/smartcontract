e5de51161ef2dcddc659e2042b9dacd62bb39fa2
bluecryptodev <reactdev1995@yandex.com>
2024-05-17 10:58:15 +0800

    update

--- scripts/deploy_presale.ts
+++ scripts/deploy_presale.ts
@@ -6,19 +6,19 @@ async function main() {
   const contract = await upgrades.deployProxy(
     Contract,
     // ** Ether
-    [
-      '0x4D084c9C2faA4aB089628d45D2c86F0C205d6Eb7',
-      '0x935366A68942D577842acFFED610Cc26CD90ACA0',
-      '0x55Ae632689Be114f1888a7f97DFc8a240a393AAE',
-      '0x694AA1769357215DE4FAC081bf1f309aDC325306',
-    ],
-    // ** BSC
     // [
     //   '0x4D084c9C2faA4aB089628d45D2c86F0C205d6Eb7',
-    //   '0x87Ce5D0B1Bc7D0d4ad73535CB703FD19f36b3162',
-    //   '0x987cA07Bc836f5b952cdCcE05A5084e55206637C',
-    //   '0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526',
+    //   '0x935366A68942D577842acFFED610Cc26CD90ACA0',
+    //   '0x55Ae632689Be114f1888a7f97DFc8a240a393AAE',
+    //   '0x694AA1769357215DE4FAC081bf1f309aDC325306',
     // ],
+    // ** BSC
+    [
+      '0x4D084c9C2faA4aB089628d45D2c86F0C205d6Eb7',
+      '0x38daAcdcc9C328615aAc177957289B47de36f2d6',
+      '0x987cA07Bc836f5b952cdCcE05A5084e55206637C',
+      '0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526',
+    ],
     { initializer: 'initialize', kind: 'uups' }
   );
 
